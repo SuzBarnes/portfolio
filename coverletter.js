d444d47b8b505f438f7ba4cca421990bb5bffbca
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-09-13 20:12:00 +0100

    "commit changes"

---
+++ coverletter.js
@@ -0,0 +1,22 @@
+To whom it may concern,
+
+I'm Suzie, a dentist-turned-developer. After spending a few years studying and practising dentistry, I recently enrolled on the Manchester Codes Software Development Bootcamp to make the change into software development.
+
+Some see this as a strange shift in careers, but I have the drive to learn, the ambition to succeed and the passion to problem solve.
+
+I'm now looking for my first software development role to put what I have learned into practise.
+
+I am trained in both front-end and back-end technology including HTML/CSS, Javascript, APIs, MySQL, TDD, React and much more.
+
+I want to stand out, not because I've made an unusual shift in careers, but because I think I've got the potential to make a difference at Bosch.
+
+As a dentist, I work in a team and solve difficult problems under pressure. I translate complex jargon into layman's terms, and manage tricky stakeholders. I'm the type of person that builds others' confidence in themselves, myself and in my team.
+
+I am honest, hard-working and focused. I know how to run on time and communicate with the team if there are any problems along the way.
+
+I would absolutely love to work at Bosch, to have a large team of Software Engineers who I can learn from, support and absorb their expertise.
+
+I'd love the opportunity to tell you a bit more about myself, and explain why I think I'd be a great fit for the team at Bosch Group.
+
+Best wishes,
+Suzannah
